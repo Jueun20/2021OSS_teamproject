bc8ef505e5383a02f59be0ee236a68047b622428
Jueun20 <79799635+Jueun20@users.noreply.github.com>
2021-05-06 22:20:13 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -1,3 +1,19 @@
+int createItem(Item *p){
+        printf("날짜: ");
+        scanf(" %d", &p -> date);
+                
+        printf("분류[교통비/문화생활/생활용품/식비/의류]: ");
+        scanf(" %s", p -> cate);
+
+        printf("가격: ");
+        scanf(" %d", &p -> price);
+
+        printf("내용: ");
+        scanf(" %[^\n]s", p -> name);
+
+        return 1;
+}
+
 //delete 함수
 int deleteProduct(Item *p){
         p->price = -1;
