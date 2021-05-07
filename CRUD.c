e299e96e493ed71522580ab33c3c84041c03bfd4
Jueun20 <79799635+Jueun20@users.noreply.github.com>
2021-05-07 22:44:01 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -18,7 +18,7 @@ int createItem(Item *p){
 
 //readItem()
 void readItem(Item p){
-        printf("%d  ||  %s  ||  %d  ||  %s \n", p.data, p.cate, p.price, p.name);
+        printf("\t%d  ||  %s  ||  %d  ||  %s \n", p.data, p.cate, p.price, p.name);
         printf("\n");
 }
 
