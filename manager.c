a424d006ef446ffd6c36f44bdbea67b8075ea5c3
Jueun20 <79799635+Jueun20@users.noreply.github.com>
2021-05-14 18:55:54 +0900

    Update manager.c

--- manager.c
+++ manager.c
@@ -88,7 +88,6 @@ int loadData(Item *p){
                         fscanf(fp, "%d", &p[i].price);
                         fscanf(fp, "%d", &p[i].date);
 
-                                p[i].starCount = (int)p[i].star;
                 }
                 fclose(fp);
                 printf("\n");
