0c651eb14d7b7cd6b6d204445fc60f3d45fefc6e
20Yiju <22000690@handong.edu>
2021-05-17 16:42:25 +0900

    update manager.c

--- main.c
+++ main.c
@@ -12,48 +12,47 @@ int main(void){
     menu = selectMenu();
     if(menu == 0) break;
     if(menu == 1){
-	
-listItem(ilist, index);
-      readItem(ilist[0]);
+	listItem(ilist, index);
+	readItem(ilist[0]);
     }
     else if(menu == 2){
-      count += createItem(&ilist[index]);
-      index++;
+	count += createItem(&ilist[index]);
+	index++;
     }
     else if(menu == 3){
-      int no = selectDataNum(ilist, index);
-      if(no > 0){
-        updateItem(&ilist[no-1]);
-      }
-      else{
-        printf("=> 취소되었습니다!:->\n");
-        continue;
-      }
+	int no = selectDataNum(ilist, index);
+	if(no > 0){
+		updateItem(&ilist[no-1]);
+      	}
+	else{
+		printf("=> 취소되었습니다!:->\n");
+        	continue;
+      	}
     }
     else if(menu == 4){
-      int no = selectDataNum(ilist, index);
-      if(no > 0) {
-        printf("정말로 삭제하시겠습니까?(삭제: 1) ");
-      }
-      int delok;
-      scanf("%d", &delok);
-      if(delok == 1) {
-        deleteItem(&ilist[no-1]);
-        count--;
-        printf("=> 삭제되었습니다 :-)\n");
-      }
+	int no = selectDataNum(ilist, index);
+	if(no > 0) {
+		printf("정말로 삭제하시겠습니까?(삭제: 1) ");
+      	}
+	int delok;
+	scanf("%d", &delok);
+	if(delok == 1) {
+		deleteItem(&ilist[no-1]);
+        	count--;
+        	printf("=> 삭제되었습니다 :-)\n");
+      	}
     }
     else if(menu == 5) {
-      saveData(ilist, index);
+	saveData(ilist, index);
     }
     else if(menu == 6) {
-      searchName(ilist, index);
+	searchName(ilist, index);
     }
     else if(menu == 7) {
-      searchCate(ilist, index);
+	searchCate(ilist, index);
     }
     else if(menu == 8) {
-      searchDate(ilist, index);
+	searchDate(ilist, index);
     }
   }
   printf("종료되었습니다:-)\n");
